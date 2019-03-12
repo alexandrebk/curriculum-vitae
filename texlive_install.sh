9d5770d5cdbd721299797163d8a09b8f4acd084b
Cyril Bouvier <cyril.bouvier@lirmm.fr>
2019-03-12 19:33:52 +0100

    debug textlive_install.sh

--- texlive_install.sh
+++ texlive_install.sh
@@ -25,7 +25,7 @@ fi
 # Needed for any use of texlua even if not testing LuaTeX
 tlmgr install luatex
 
-tlmgr search --global --file expl3.sty
+tlmgr search --global --file microtype.sty
 
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
